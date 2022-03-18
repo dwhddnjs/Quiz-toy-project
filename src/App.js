ac17265640a684bccb039b96cd0050c9154cb0f7
dwhddnjs <per1215@naver.com>
2022-03-19 00:49:55 +0900

    Add decrease fun

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import useStore from "./store";
 function App() {
   const { count } = useStore();
   const increase = useStore((state) => state.inc);
+  const decrease = useStore((state) => state.dec);
+
   return (
     <div className="App">
-      <div>{count}</div>
+      <div>나이 {count}</div>
       <Counter />
       <button onClick={increase}>click me</button>
-      {/* <button onClick={des}>click me</button> */}
+      <button onClick={decrease}>click me</button>
     </div>
   );
 }
